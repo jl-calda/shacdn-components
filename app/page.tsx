b860ca9b383abc297dd95dd3d10230bebe014ec6
Calda Joselito Jr Lopez <joselito.calda@DSB.com>
2023-09-20 17:52:03 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import EditableText from "@/components/ui/editable-text";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <EditableText />
+    </main>
+  );
+}
---
+++ components/ui/editable-text.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+interface EditableTextProps {
+  initialData: {
+    value: string;
+  };
+  apiEndpoint: string;
+}
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+const EditableText: React.FC<EditableTextProps> = ({
+  initialData,
+  apiEndpoint,
+}) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {};
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="shadcn"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default EditableText;
